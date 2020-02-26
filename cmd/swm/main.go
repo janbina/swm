903c43fbe64245c2f80ce9405da8365b09b5ac18
Jan Bína <binajohny@gmail.com>
2020-02-26 17:03:43 +0100

    Add replace flag

--- cmd/swm/main.go
+++ cmd/swm/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"github.com/BurntSushi/xgbutil"
 	"log"
 )
 
 func main() {
+
+	replace := flag.Bool("replace", false, "whether swm should replace current wm")
+	flag.Parse()
 
 	X, err := xgbutil.NewConn()
 	if err != nil {
@@ -13,7 +17,7 @@ func main() {
 	}
 	defer X.Conn().Close()
 
-	if err := takeWmOwnership(X, true); err != nil {
+	if err := takeWmOwnership(X, *replace); err != nil {
 		log.Fatalf("Cannot take wm ownership: %s", err)
 	}
 }
