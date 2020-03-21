8a3aa0dbe5fef33bc933c270e9cdcb5d3b168cb1
Jan Bína <binajohny@gmail.com>
2020-03-21 02:24:24 +0100

    Ditch the reparenting for now and use simple borders

--- window/window.go
+++ window/window.go
@@ -9,15 +9,12 @@ import (
 	"github.com/BurntSushi/xgbutil/xrect"
 	"github.com/BurntSushi/xgbutil/xwindow"
 	"github.com/janbina/swm/cursors"
-	"github.com/janbina/swm/decoration"
 	"github.com/janbina/swm/util"
 	"log"
 )
 
 type Window struct {
-	parent      *xwindow.Window
 	win         *xwindow.Window
-	decorations decoration.Decorations
 	protocols   []string
 	moveState   *MoveState
 }
@@ -41,17 +38,14 @@ func New(x *xgbutil.XUtil, xWin xproto.Window) *Window {
 		log.Println("Wm protocols not set")
 	}
 
-	w := xwindow.New(x, xWin)
-
-	p, err := createParent(w)
-
-	decorations, _ := decoration.CreateBorders(p, 2, 0xff00ff)
-
 	win := xwindow.New(x, xWin)
 
+	err = util.SetBorder(win, 3, 0xff00ff)
+	if err != nil {
+		log.Printf("Cannot set window border")
+	}
+
 	window := &Window{
-		parent:      p,
-		decorations: decorations,
 		win:         win,
 		protocols:   protocols,
 		moveState: &MoveState{
@@ -61,50 +55,18 @@ func New(x *xgbutil.XUtil, xWin xproto.Window) *Window {
 		},
 	}
 
-	decW := decorations.WidthNeeded()
-	decH := decorations.HeightNeeded()
-	g, _ := util.GeometryIncludingBorder(win)
-	g.WidthSet(g.Width() + decW)
-	g.HeightSet(g.Height() + decH)
-	window.MoveResize(g.Pieces())
-
 	return window
 }
 
-// Create parent window for [win]
-func createParent(win *xwindow.Window) (*xwindow.Window, error) {
-	X := win.X
-
-	parent, err := xwindow.Create(X, X.RootWin())
-	if err != nil {
-		return nil, err
-	}
-	// TODO: red bg so we can easily see if we are off a bit, maybe set to something else later
-	parent.Change(xproto.CwBackPixel, 0xff0000)
-
-	// Set window border to 0, as we will either use our own borders or we don't want any
-	err = util.SetBorder(win, 5, 0x00ffff)
-	if err != nil {
-		return nil, err
-	}
-
-	err = xproto.ReparentWindowChecked(X.Conn(), win.Id, parent.Id, 0, 0).Check()
-	if err != nil {
-		return nil, err
-	}
-
-	return parent, nil
-}
-
 // Unique id of this window
-// It is backed by parent window Id, but we don't want to expose that
+// It is backed by window Id, but we don't want to expose that
 // so all manipulations which needs xproto.Window must happen here
 func (w *Window) Id() uint32 {
-	return uint32(w.parent.Id)
+	return uint32(w.win.Id)
 }
 
 func (w *Window) Geometry() (xrect.Rect, error) {
-	return w.parent.Geometry()
+	return w.win.Geometry()
 }
 
 func (w *Window) Listen(evMasks ...int) error {
@@ -112,8 +74,6 @@ func (w *Window) Listen(evMasks ...int) error {
 }
 
 func (w *Window) Map() {
-	w.parent.Map()
-	w.decorations.Map()
 	w.win.Map()
 }
 
@@ -146,16 +106,11 @@ func (w *Window) Resize(d Directions) {
 }
 
 func (w *Window) Move(x, y int) {
-	w.parent.Move(x, y)
+	w.win.Move(x, y)
 }
 
 func (w *Window) MoveResize(x, y, width, height int) {
-	w.parent.MoveResize(x, y, width, height)
-	rect := w.decorations.ApplyRect(xrect.New(0, 0, width, height))
-	borderW := util.GetBorderWidth(w.win)
-	rect.WidthSet(rect.Width() - 2 * int(borderW))
-	rect.HeightSet(rect.Height() - 2 * int(borderW))
-	w.win.MoveResize(rect.Pieces())
+	w.win.MoveResize(x, y, width, height)
 }
 
 func (w *Window) HasProtocol(x string) bool {
@@ -168,14 +123,6 @@ func (w *Window) HasProtocol(x string) bool {
 }
 
 func (w *Window) WasUnmapped() {
-	X := w.win.X
-	w.decorations.Unmap()
-	w.parent.Unmap()
-
-	xproto.ReparentWindowChecked(X.Conn(), w.win.Id, X.RootWin(), 0, 0).Check()
-
-	w.decorations.Destroy()
-	w.parent.Destroy()
 }
 
 func (w *Window) DragMoveBegin(rx, ry, ex, ey int) bool {
@@ -211,7 +158,7 @@ func (w *Window) SetupMouseEvents(moveShortcut string) {
 	X := w.win.X
 
 	// Detach old events
-	mousebind.Detach(X, w.parent.Id)
+	mousebind.Detach(X, w.win.Id)
 
 	if _, _, err := mousebind.ParseString(X, moveShortcut); err != nil {
 		return
@@ -228,5 +175,5 @@ func (w *Window) SetupMouseEvents(moveShortcut string) {
 		func(X *xgbutil.XUtil, rx, ry, ex, ey int) {
 			w.DragMoveEnd(rx, ry, ex, ey)
 		})
-	mousebind.Drag(X, X.Dummy(), w.parent.Id, moveShortcut, true, dStart, dStep, dEnd)
+	mousebind.Drag(X, X.Dummy(), w.win.Id, moveShortcut, true, dStart, dStep, dEnd)
 }
