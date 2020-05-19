5800c838e676bb8039a098fddcfda16800e900e8
Jan Bína <binajohny@gmail.com>
2020-05-20 00:02:26 +0200

    Remove keybinding altogether

--- cmd/swm/main.go
+++ cmd/swm/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"flag"
 	"github.com/BurntSushi/xgbutil"
-	"github.com/BurntSushi/xgbutil/keybind"
-	"github.com/BurntSushi/xgbutil/xevent"
 	"github.com/janbina/swm/internal/communication"
 	"github.com/janbina/swm/internal/config"
 	"github.com/janbina/swm/internal/windowmanager"
 	"log"
-	"os/exec"
 )
 
 func main() {
@@ -37,12 +34,6 @@ func main() {
 	config.FindAndRunSwmrc(*customConfig)
 
 	windowmanager.ManageExistingClients()
-
-	keybind.KeyPressFun(
-		func(X *xgbutil.XUtil, ev xevent.KeyPressEvent) {
-			exec.Command("xterm").Start()
-		},
-	).Connect(windowmanager.X, windowmanager.Root.Id, "control-Mod1-return", true)
 
 	windowmanager.Run()
 }
