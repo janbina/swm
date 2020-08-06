36c0bb3ab7aedb7bb68da5a9011cdb295f80ea3c
Jan Bína <binajohny@gmail.com>
2020-08-06 13:41:08 +0200

    Refactor groups a bit

--- internal/groupmanager/group.go
+++ internal/groupmanager/group.go
@@ -20,6 +20,26 @@ func createGroup(name string) *group {
 	}
 }
 
+func (g *group) addWindow(window xproto.Window) {
+	g.windows[window] = true
+}
+
+func (g *group) removeWindow(window xproto.Window) {
+	delete(g.windows, window)
+}
+
+func (g *group) removeAllWindows() {
+	g.windows = map[xproto.Window]bool{}
+}
+
+func (g *group) getWindows() []xproto.Window {
+	wins := make([]xproto.Window, 0, len(g.windows))
+	for win := range g.windows {
+		wins = append(wins, win)
+	}
+	return wins
+}
+
 func (g *group) isVisible() bool {
 	return g.shownTimestamp > 0
 }
